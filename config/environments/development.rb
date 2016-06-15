8eb1138fa71d706559ec8ca996a6b81ea26ea78b
Alan Willms <dyulax@gmail.com>
2016-06-14 21:11:42 -0300

    Allow any Origin (CORS) in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow any origin
+  config.action_dispatch.default_headers.merge!({
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => '*'
+  })
 end
