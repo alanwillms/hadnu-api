c4446fdf910d4cd28673cd2f8f9382bd92625648
Alan Willms <dyulax@gmail.com>
2016-07-01 15:12:27 -0300

    Fix section uploaded images path

--- app/serializers/sections/show_serializer.rb
+++ app/serializers/sections/show_serializer.rb
@@ -1,4 +1,6 @@
 class Sections::ShowSerializer < SectionSerializer
+  include Rails.application.routes.url_helpers
+
   attributes :text, :seo_keywords, :seo_description, :published_at, :next,
     :previous
 
@@ -8,5 +10,17 @@ class Sections::ShowSerializer < SectionSerializer
 
   def next
     object.next ? SectionSerializer.new(object.next) : nil
+  end
+
+  def text
+    text = object.text
+    if text
+      base_url = publication_section_url(object.publication, object) + '/images/'
+      text = text.gsub('<livrourl>/', base_url)
+      text = text.gsub('<livrourl>', base_url)
+      text = text.gsub(/\/library\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\//, base_url)
+      text = text.gsub(/http\:\/\/hadnu\.org\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\//, base_url)
+    end
+    text
   end
 end
