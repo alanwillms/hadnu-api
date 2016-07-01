837273bf0536dca3fda87155fcfe2959f4462ea3
Alan Willms <dyulax@gmail.com>
2016-07-01 10:44:25 -0300

    Add has_text to Sections#index API

--- app/serializers/section_serializer.rb
+++ app/serializers/section_serializer.rb
@@ -1,3 +1,7 @@
 class SectionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :parent_id, :position
+  attributes :id, :title, :parent_id, :position, :has_text
+
+  def has_text
+    object.text.to_s.strip != ''
+  end
 end
