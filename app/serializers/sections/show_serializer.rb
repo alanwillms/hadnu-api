ce69805acfb75c0af3a928c046ea83af4a9e465c
Alan Willms <dyulax@gmail.com>
2016-07-01 10:55:31 -0300

    Section previous and next now use the serializer

--- app/serializers/sections/show_serializer.rb
+++ app/serializers/sections/show_serializer.rb
@@ -1,4 +1,12 @@
 class Sections::ShowSerializer < SectionSerializer
   attributes :text, :seo_keywords, :seo_description, :published_at, :next,
     :previous
+
+  def previous
+    object.previous ? SectionSerializer.new(object.previous) : nil
+  end
+
+  def next
+    object.next ? SectionSerializer.new(object.next) : nil
+  end
 end
