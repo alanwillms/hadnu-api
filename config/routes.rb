306c49087a4aa905970b6c0b4c99726f6b2b44fc
Alan Willms <dyulax@gmail.com>
2016-07-04 00:28:16 -0300

    Remove deprecated CORS hack

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  # Options requests
-  match '*path', via: [:options], to: lambda { |_|
-    [
-      204,
-      {
-        'Content-Type' => 'text/plain',
-        'Access-Control-Allow-Origin' => '*',
-        'Access-Control-Request-Method' => '*',
-        'Access-Control-Allow-Headers' => 'Authorization, Content-Type, X-Total, X-Per-Page, X-Page',
-        'Access-Control-Expose-Headers' => 'Authorization, Content-Type, X-Total, X-Per-Page, X-Page'
-      },
-      []
-    ]
-  }
-
   # Authentication
   post 'auth/:provider/callback' => 'omniauth#create'
   post 'user_token' => 'user_token#create'
