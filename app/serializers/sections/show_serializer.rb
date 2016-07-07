bad082f3a08bd8619123f9a1282ba18e776778d7
Alan Willms <dyulax@gmail.com>
2016-07-07 12:37:22 -0300

    Add source to Sections#show API

--- app/serializers/sections/show_serializer.rb
+++ app/serializers/sections/show_serializer.rb
@@ -2,7 +2,7 @@ class Sections::ShowSerializer < SectionSerializer
   include Rails.application.routes.url_helpers
 
   attributes :text, :seo_keywords, :seo_description, :published_at, :next,
-             :previous
+             :previous, :source
 
   def previous
     object.previous ? SectionSerializer.new(object.previous) : nil
@@ -15,14 +15,26 @@ class Sections::ShowSerializer < SectionSerializer
   def text
     return nil unless object.text
     text = object.text
-    base_url = publication_section_url(object.publication, object) + '/images/'
-    text = text.gsub('images/symbols/', 'static/images/symbols/')
-    text = text.gsub('images/layout/', 'static/images/layout/')
-    text = text.gsub('<livrourl>/', base_url)
-    text = text.gsub('<livrourl>', base_url)
-    text = text.gsub(/\/library\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\//, base_url)
-    text = text.gsub(/http\:\/\/hadnu\.org\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\//, base_url)
-    text = text.gsub(/\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\//, base_url)
+    text_replacements.each { |from, to| text = text.gsub(from, to) }
     text
+  end
+
+  private
+
+  def text_replacements
+    {
+      'images/symbols/' => 'static/images/symbols/',
+      'images/layout/' => 'static/images/layout/',
+      '<livrourl>/' => base_url,
+      '<livrourl>' => base_url,
+      /\/library\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\// => base_url,
+      /http\:\/\/hadnu\.org\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\// => base_url,
+      /\/sections\-images\/view\?section\_id\=\d+\&amp\;file\=\// => base_url,
+      %r{http\://hadnu.org/sections-images/view\?section\_id\=\d+\&amp\;file\=} => base_url
+    }
+  end
+
+  def base_url
+    @base_url ||= publication_section_url(object.publication, object) + '/images/'
   end
 end
