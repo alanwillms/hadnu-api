969d5630c910b3eabf05198146b28979a28d016f
Alan Willms <dyulax@gmail.com>
2016-07-09 17:30:30 -0300

    Add UserRegistrations specs

---
+++ spec/controllers/user_registrations_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe UserRegistrationsController do
+  describe '#create' do
+    context 'with valid data' do
+      before :each do
+        payload = {
+          user_registration: {
+            name: 'John Doe',
+            login: 'johndoe',
+            password: 's3cr3t',
+            email: 'john@doe.com'
+          }
+        }
+        post :create, params: payload
+      end
+
+      it 'has a 201 status' do
+        expect(response.status).to be(201)
+      end
+
+      it 'renders user data' do
+        expect(json_response).to include(
+          'login' => 'johndoe'
+        )
+      end
+    end
+
+    context 'with invalid data' do
+      before :each do
+        payload = {
+          user_registration: {
+            name: 'John Doe',
+            login: '',
+            password: 's3cr3t',
+            email: 'john@doe.com'
+          }
+        }
+        post :create, params: payload
+      end
+
+      it 'has a 422 status' do
+        expect(response.status).to be(422)
+      end
+
+      it 'renders errors' do
+        expect(json_response).to include(
+          'login' => ['can\'t be blank']
+        )
+      end
+    end
+  end
+end
