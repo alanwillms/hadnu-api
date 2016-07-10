210a723995449ef093cf6d09c329348ef48b5866
Alan Willms <dyulax@gmail.com>
2016-07-09 22:35:59 -0300

    UserPolicy specs

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,11 +1,5 @@
 class UserPolicy < ApplicationPolicy
   def show?
-    !!user
-  end
-
-  class Scope < Scope
-    def resolve
-      scope
-    end
+    authenticated_user?
   end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe UserPolicy do
-
+describe UserPolicy do
+  let(:normal_user) { create(:user) }
+  let(:admin_user) { create(:admin_user) }
+  let(:policy) { described_class }
   let(:user) { User.new }
 
-  subject { described_class }
-
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'denies access to guest user' do
+      expect(policy).not_to permit(nil, User)
+    end
+
+    it 'allow access to normal user and admin user' do
+      expect(policy).to permit(normal_user, User)
+      expect(policy).to permit(admin_user, User)
+    end
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :index?, :new?, :create?, :edit?, :update?, :destroy? do
+    it 'denies access to guest, normal and admin user' do
+      expect(policy).not_to permit(nil, user)
+      expect(policy).not_to permit(normal_user, user)
+      expect(policy).not_to permit(admin_user, user)
+    end
   end
 end
