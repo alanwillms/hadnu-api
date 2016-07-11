e8e130a7c273fd76761c1972211df718cb42feee
Alan Willms <dyulax@gmail.com>
2016-07-11 17:15:33 -0300

    Category specs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,17 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :publications
-
   has_attached_file :banner,
                     styles: { card: '630x354#' },
                     default_url: '/images/:style/missing.png'
+
+  validates :name,
+            presence: true,
+            length: { maximum: 255 },
+            uniqueness: { case_sensitive: false }
+  validates :hits,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, presence: true, length: { maximum: 2000 }
   validates_attachment_content_type :banner, content_type: %r{\Aimage\/.*\Z}
-  validates :name, presence: true
-  validates :description, presence: true
+  validates_attachment_size :banner, less_than: 2.megabytes
 end
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Category do
+  context 'validations' do
+    before { create(:category) }
+
+    it { should have_and_belong_to_many(:publications) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:hits) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+    it { should validate_length_of(:name).is_at_most(255) }
+    it { should validate_length_of(:description).is_at_most(2000) }
+    it { should have_attached_file(:banner) }
+    it { should validate_attachment_size(:banner).less_than(2.megabytes) }
+    it do
+      should validate_attachment_content_type(:banner)
+        .allowing('image/png', 'image/gif', 'image/jpg')
+        .rejecting('text/plain', 'text/xml')
+    end
+    it do
+      should validate_numericality_of(:hits)
+        .only_integer
+        .is_greater_than_or_equal_to(0)
+    end
+  end
+end
