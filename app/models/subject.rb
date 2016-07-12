70dfdceebbd6dc4ec945dbf6d0ff2f97850b16a5
Alan Willms <dyulax@gmail.com>
2016-07-11 23:30:44 -0300

    Subject specs

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,12 @@
 class Subject < ApplicationRecord
+  COLOR_FORMAT = /\A([A-Fa-f0-9]{6})\z/i
+
   has_many :discussions
+
+  validates :name, presence: true, uniqueness: true
+  validates :label_text_color, presence: true, format: { with: COLOR_FORMAT }
+  validates :label_background_color,
+            presence: true,
+            uniqueness: true,
+            format: { with: COLOR_FORMAT }
 end
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -3,5 +3,19 @@ require 'rails_helper'
 describe Subject do
   context 'validations' do
     before { create(:subject) }
+
+    it { should have_many(:discussions) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:label_background_color) }
+    it { should validate_presence_of(:label_text_color) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:label_background_color) }
+    it { should allow_value('FF0000').for(:label_background_color) }
+    it { should allow_value('FFCC33').for(:label_text_color) }
+    it { should_not allow_value('F00').for(:label_background_color) }
+    it { should_not allow_value('FC3').for(:label_text_color) }
+    it { should_not allow_value('foo').for(:label_background_color) }
+    it { should_not allow_value('bar').for(:label_text_color) }
   end
 end
