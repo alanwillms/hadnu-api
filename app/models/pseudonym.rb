ed47237b0ed46fa9a89b2a4994d8872ca89951df
Alan Willms <dyulax@gmail.com>
2016-07-11 22:14:18 -0300

    Pseudonym specs

--- app/models/pseudonym.rb
+++ app/models/pseudonym.rb
@@ -1,4 +1,11 @@
 class Pseudonym < ApplicationRecord
   belongs_to :author
   belongs_to :user
+
+  validates :author, presence: true
+  validates :user, presence: true
+  validates :name,
+            presence: true,
+            length: { maximum: 255 },
+            uniqueness: { scope: :author_id }
 end
--- spec/models/pseudonym_spec.rb
+++ spec/models/pseudonym_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 describe Pseudonym do
   context 'validations' do
     before { create(:pseudonym) }
+
+    it { should belong_to(:author) }
+    it { should belong_to(:user) }
+
+    it { should validate_presence_of(:author) }
+    it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(255) }
+    it { should validate_uniqueness_of(:name).scoped_to(:author_id) }
   end
 end
