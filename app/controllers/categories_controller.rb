3f453d845e0a5bfeabab69b7b4d89455adffc22f
Alan Willms <dyulax@gmail.com>
2016-07-20 19:27:23 -0300

    Configure ETAG cache for categories API

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,30 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: :show
-
   def index
     authorize Category
-    paginate json: scope.order(:name).all
+    paginate json: scope.order(:name).all if stale? etag: index_etag
   end
 
   def show
-    authorize @category
-    render json: @category
+    authorize category
+    render json: category if stale? etag: show_etag
   end
 
   private
 
-  def set_category
-    @category = scope.find(params[:id])
+  def index_etag
+    scope.maximum(:updated_at).to_s + ',' + scope.count.to_s
+  end
+
+  def show_etag
+    [
+      category.updated_at.to_s,
+      policy_scope(category.publications).count.to_s,
+      policy_scope(category.publications).maximum(:updated_at)
+    ].join(',')
+  end
+
+  def category
+    @category ||= scope.find(params[:id])
   end
 
   def scope
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -16,6 +16,35 @@ describe CategoriesController do
         'name' => category.name
       )
     end
+
+    context 'cache' do
+      let(:category) do
+        create(:category_with_publications, updated_at: 1.hour.ago)
+      end
+
+      before(:each) do
+        category
+        get :index
+        set_request_etag_headers
+      end
+
+      it 'has a 304 status' do
+        get :index
+        expect(response.status).to be(304)
+      end
+
+      it 'expires if a category gets updated' do
+        category.touch
+        get :index
+        expect(response.status).to be(200)
+      end
+
+      it 'expires if a new category is created' do
+        create(:category_with_publications)
+        get :index
+        expect(response.status).to be(200)
+      end
+    end
   end
 
   describe '#show' do
@@ -26,6 +55,47 @@ describe CategoriesController do
         'id' => category.id,
         'name' => category.name
       )
+    end
+
+    context 'cache' do
+      let(:category) do
+        record = create(:category_with_publications, updated_at: 1.hour.ago)
+        publication = record.publications.first
+        publication.updated_at = 1.hour.ago
+        publication.save
+        record
+      end
+
+      before(:each) do
+        get :show, params: { id: category.id }
+        set_request_etag_headers
+      end
+
+      it 'has a 304 status' do
+        get :show, params: { id: category.id }
+        expect(response.status).to be(304)
+      end
+
+      it 'expires if gets updated' do
+        # Removes controller class cache
+        controller.instance_variable_set(:@category, nil)
+        category.touch
+        get :show, params: { id: category.id }
+        expect(response.status).to be(200)
+      end
+
+      it 'expires if one of his publications get updated' do
+        publication = category.publications.first
+        publication.touch
+        get :show, params: { id: category.id }
+        expect(response.status).to be(200)
+      end
+
+      it 'expires if a new publication is created written by him or her' do
+        create(:category_publication, category: category)
+        get :show, params: { id: category.id }
+        expect(response.status).to be(200)
+      end
     end
   end
 end
