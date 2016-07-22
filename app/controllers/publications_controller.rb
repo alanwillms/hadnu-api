431a8b9540ea93d9721f23a750110f54ea340632
Alan Willms <dyulax@gmail.com>
2016-07-22 17:54:03 -0300

    Configure ETAG cache for publications API

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,31 +1,61 @@
 class PublicationsController < ApplicationController
-  before_action :set_publication, only: :show
-
   def index
     authorize Publication
-    paginate json: publications.recent_first.all
+    paginate json: publications.recent_first.all if stale? etag: index_etag
   end
 
   def show
-    authorize @publication
-    render json: @publication, serializer: Publications::ShowSerializer
+    authorize publication
+    if stale? etag: show_etag
+      render json: publication, serializer: Publications::ShowSerializer
+    end
   end
 
   private
 
+  def index_etag
+    [
+      publications.maximum(:updated_at).to_s,
+      publications.count.to_s,
+      request[:page].to_s,
+      params[:author_id].to_s,
+      params[:category_id].to_s
+    ].join(',')
+  end
+
+  def show_etag
+    [
+      publication.updated_at.to_s,
+      publication.sections.count.to_s,
+      publication.sections.maximum(:updated_at).to_s
+    ].join(',')
+  end
+
   def publications
-    scope = if params[:category_id]
-      Category.find(params[:category_id]).publications
-    elsif params[:author_id]
-      Author.find(params[:author_id]).publications
-    else
-      Publication
-    end
+    scope = unfiltered_publications
     scope.random_order if params[:order] == 'random'
     policy_scope scope
   end
 
-  def set_publication
-    @publication = Publication.find(params[:id])
+  def unfiltered_publications
+    if category
+      category.publications
+    elsif author
+      author.publications
+    else
+      Publication
+    end
+  end
+
+  def author
+    @author ||= Author.find(params[:author_id]) if params[:author_id]
+  end
+
+  def category
+    @category ||= Category.find(params[:category_id]) if params[:category_id]
+  end
+
+  def publication
+    @publication ||= Publication.find(params[:id])
   end
 end
--- spec/controllers/publications_controller_spec.rb
+++ spec/controllers/publications_controller_spec.rb
@@ -55,6 +55,35 @@ describe PublicationsController do
       get :index
       expect(json_response).to eq([])
     end
+
+    context 'cache' do
+      let(:publication) do
+        create(:publication, updated_at: 1.hour.ago)
+      end
+
+      before(:each) do
+        publication
+        get :index
+        set_request_etag_headers
+      end
+
+      it 'has a 304 status' do
+        get :index
+        expect(response.status).to be(304)
+      end
+
+      it 'expires if a publication gets updated' do
+        publication.touch
+        get :index
+        expect(response.status).to be(200)
+      end
+
+      it 'expires if a new publication is created' do
+        create(:publication)
+        get :index
+        expect(response.status).to be(200)
+      end
+    end
   end
 
   describe '#show' do
@@ -65,6 +94,50 @@ describe PublicationsController do
         'id' => publication.id,
         'title' => publication.title
       )
+    end
+
+    context 'cache' do
+      let(:publication) do
+        create(:publication, updated_at: 1.hour.ago)
+      end
+
+      let(:section) do
+        create(:section, publication: publication, updated_at: 1.hour.ago)
+      end
+
+      before(:each) do
+        section
+        get :show, params: { id: publication.id }
+        set_request_etag_headers
+      end
+
+      it 'has a 304 status' do
+        get :show, params: { id: publication.id }
+        expect(response.status).to be(304)
+      end
+
+      it 'expires if gets updated' do
+        # Removes controller class cache
+        controller.instance_variable_set(:@publication, nil)
+        publication.touch
+        get :show, params: { id: publication.id }
+        expect(response.status).to be(200)
+      end
+
+      it 'expires if one of its sections gets updated' do
+        section.touch
+        get :show, params: { id: publication.id }
+        expect(response.status).to be(200)
+      end
+
+      it 'expires if a new section is created for it' do
+        create(
+          :section,
+          publication: publication, root: section, parent: section
+        )
+        get :show, params: { id: publication.id }
+        expect(response.status).to be(200)
+      end
     end
   end
 end
