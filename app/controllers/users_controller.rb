ab624d2625a0a7f2dc0ce7e046819abb92425b53
Alan Willms <dyulax@gmail.com>
2016-07-22 18:05:47 -0300

    Configure user ETAG cache

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,14 @@ class UsersController < ApplicationController
 
   def show
     authorize user
-    render json: user
+    render json: user if stale? etag: show_etag
   end
 
   private
+
+  def show_etag
+    user.updated_at.to_s
+  end
 
   def user
     @user ||= User.find(params[:id])
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,14 +2,38 @@ require 'rails_helper'
 
 describe UsersController do
   describe '#show' do
-    it 'display user' do
+    let(:user) { create(:user, updated_at: 1.hour.ago) }
+    before(:each) do
       authenticate
-      user = create(:user)
       get :show, params: { id: user.id }
+    end
+
+    it 'display user' do
       expect(json_response).to include(
         'id' => user.id,
         'login' => user.login
       )
+    end
+
+    it 'has a 200 status' do
+      expect(response.status).to be(200)
+    end
+
+    context 'cache' do
+      before(:each) { set_request_etag_headers }
+
+      it 'has a 304 status' do
+        get :show, params: { id: user.id }
+        expect(response.status).to be(304)
+      end
+
+      it 'expires if gets updated' do
+        # Removes controller class cache
+        controller.instance_variable_set(:@user, nil)
+        user.touch
+        get :show, params: { id: user.id }
+        expect(response.status).to be(200)
+      end
     end
   end
 end
