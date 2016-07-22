3b0759b3166a38e44d40c9903112d021c59bf542
Alan Willms <dyulax@gmail.com>
2016-07-21 21:30:24 -0300

    Configure featured publications ETAG browser cache

--- app/controllers/featured_publications_controller.rb
+++ app/controllers/featured_publications_controller.rb
@@ -1,6 +1,21 @@
 class FeaturedPublicationsController < ApplicationController
   def index
     authorize Publication
-    render json: policy_scope(Publication).featured.recent_first.all
+    if stale? etag: index_etag
+      render json: featured_publications.recent_first.all
+    end
+  end
+
+  private
+
+  def index_etag
+    [
+      featured_publications.maximum(:updated_at).to_s,
+      featured_publications.count.to_s
+    ].join(',')
+  end
+
+  def featured_publications
+    policy_scope(Publication).featured
   end
 end
--- spec/controllers/featured_publications_controller_spec.rb
+++ spec/controllers/featured_publications_controller_spec.rb
@@ -2,13 +2,48 @@ require 'rails_helper'
 
 describe FeaturedPublicationsController do
   describe '#index' do
+    let(:featured_publication) do
+      create(:publication, featured: true, updated_at: 1.hour.ago)
+    end
+
     it 'lists featured publications' do
-      publication = create(:publication, featured: true)
+      featured_publication
       get :index
       expect(json_response.first).to include(
-        'id' => publication.id,
-        'title' => publication.title
+        'id' => featured_publication.id,
+        'title' => featured_publication.title
       )
+    end
+
+    it 'has a 200 status' do
+      featured_publication
+      get :index
+      expect(response.status).to be(200)
+    end
+
+    context 'cache' do
+      before(:each) do
+        featured_publication
+        get :index
+        set_request_etag_headers
+      end
+
+      it 'has a 304 status' do
+        get :index
+        expect(response.status).to be(304)
+      end
+
+      it 'expires if a publication gets updated' do
+        featured_publication.touch
+        get :index
+        expect(response.status).to be(200)
+      end
+
+      it 'expires if a new publication is created' do
+        create(:publication, featured: true)
+        get :index
+        expect(response.status).to be(200)
+      end
     end
   end
 end
