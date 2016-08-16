01b742af79a0e3e09abc64d3bc1524d322fadc60
Alan Willms <dyulax@gmail.com>
2016-08-16 13:49:46 -0300

    Rise RackAttack limits

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,8 +8,8 @@ class Rack::Attack
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
 
-  # Allow an IP address to make 5 requests every 5 seconds
-  throttle('req/ip', limit: 5, period: 5) do |req|
+  # Allow an IP address to make "limit" requests every "period" seconds
+  throttle('req/ip', limit: 120, period: 120) do |req|
     req.ip unless req.path.starts_with?('/system')
   end
 
