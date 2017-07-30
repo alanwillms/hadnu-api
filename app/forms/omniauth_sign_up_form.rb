dd5bde42e57bd496e03e621ee29bc5bec4ccb4ab
Alan Willms <dyulax@gmail.com>
2017-07-29 23:41:59 -0300

    Refactor forms/omniauth_sign_up_form

--- app/forms/omniauth_sign_up_form.rb
+++ app/forms/omniauth_sign_up_form.rb
@@ -5,22 +5,16 @@ class OmniauthSignUpForm
   attr_accessor :token, :provider, :name, :login, :password, :registration_ip
 
   validates :token, presence: true
-  validates :provider, presence: true, inclusion: { in: %w(facebook google) }
+  validates :provider, presence: true, inclusion: { in: %w[facebook google] }
   validates :name, presence: true
   validates :login, presence: true
   validates :password, presence: true
   validates :registration_ip, presence: true
 
   def save
-    if valid_profile? && form.save
-      if provider == 'facebook'
-        form.user.facebook_id = profile[:id]
-        form.user.save
-      elsif provider == 'google'
-        form.user.google_id = profile[:id]
-        form.user.save
-      end
-      return true
+    if validate && valid_profile? && form.save
+      form.user.send("#{provider}_id=", profile[:id])
+      return form.user.save
     end
     false
   end
@@ -42,35 +36,22 @@ class OmniauthSignUpForm
   private
 
   def valid_profile?
-    if profile[:id].nil?
-      errors.add :token, I18n.t('omniauth.errors.invalid_token')
-      return false
-    end
-
-    unless profile[:verified]
-      errors.add :token, I18n.t('omniauth.errors.unverified_omniauth')
-      return false
-    end
-
-    params = {}
-
-    if provider == 'facebook'
-      params[:facebook_id] = profile[:id]
-    elsif provider == 'google'
-      params[:google_id] = profile[:id]
-    end
-
-    user = User
-           .where(params)
-           .or(User.where(email: profile[:email]))
-           .first
-
-    if user
-      errors.add :token, I18n.t('omniauth.errors.duplicated_user')
-      return false
-    end
-
+    return add_token_error('invalid_token') if profile[:id].nil?
+    return add_token_error('unverified_omniauth') unless profile[:verified]
+    return add_token_error('duplicated_user') if user_already_exists?
     true
+  end
+
+  def add_token_error(message_key)
+    errors.add :token, I18n.t("omniauth.errors.#{message_key}")
+    false
+  end
+
+  def user_already_exists?
+    User
+      .where("#{provider}_id".to_sym => profile[:id])
+      .or(User.where(email: profile[:email]))
+      .exists?
   end
 
   def form
@@ -88,33 +69,36 @@ class OmniauthSignUpForm
     return @profile if @profile
     @profile = from_facebook if provider == 'facebook'
     @profile = from_google if provider == 'google'
-    @profile ||= {}
+    @profile ||= { id: nil, verified: nil, email: nil }
+  rescue
+    @profile = { id: nil, verified: nil, email: nil }
   end
 
   def from_facebook
-    graph = Koala::Facebook::API.new(token, ENV['HADNU_FACEBOOK_SECRET'])
-    profile = graph.get_object(
+    response = facebok_api.get_object(
       'me',
       { fields: 'id,name,email,verified' },
       api_version: 'v2.6'
     )
     {
-      id: profile['id'],
-      verified: profile['verified'],
-      email: profile['email']
+      id: response['id'],
+      verified: response['verified'],
+      email: response['email']
     }
-  rescue
-    { id: nil, verified: nil, email: nil }
   end
 
   def from_google
     uri = URI('https://www.googleapis.com/oauth2/v3/tokeninfo')
     uri.query = URI.encode_www_form(id_token: token)
-    profile = JSON.parse(Net::HTTP.get(uri))
+    response = JSON.parse(Net::HTTP.get(uri))
     {
-      id: profile['sub'],
-      verified: profile['email_verified'],
-      email: profile['email']
+      id: response['sub'],
+      verified: response['email_verified'],
+      email: response['email']
     }
+  end
+
+  def facebok_api
+    Koala::Facebook::API.new(token, ENV['HADNU_FACEBOOK_SECRET'])
   end
 end
